ffe5a93dcc7fd61bdca60978c816526fb8c798c8
Nathan <nathan.fain@epitech.eu>
2024-04-22 01:37:51 +0200

    fix: adding EntitySpawnEvent listener

--- src/main/java/com/expectale/entitylimiter/listener/EntityListener.java
+++ src/main/java/com/expectale/entitylimiter/listener/EntityListener.java
@@ -5,6 +5,7 @@ import com.expectale.entitylimiter.EntityLimiter;
 import com.expectale.entitylimiter.configuration.EntityLimiterConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.vehicle.VehicleCreateEvent;
 import org.bukkit.event.vehicle.VehicleEntityCollisionEvent;
 
@@ -12,9 +13,9 @@ public class EntityListener implements Listener {
 
     @EventHandler
     public void onVehicleEntityCollision(VehicleEntityCollisionEvent event) {
-        if (Checker.isChechedChunk(event.getVehicle().getChunk())) {
+        final EntityLimiterConfiguration configuration = EntityLimiter.getINSTANCE().getConfiguration();
+        if (configuration.getEntityType().contains(event.getVehicle().getType()) && Checker.isChechedChunk(event.getVehicle().getChunk())) {
             Checker.addChechedChunk(event.getVehicle().getChunk());
-            final EntityLimiterConfiguration configuration = EntityLimiter.getINSTANCE().getConfiguration();
             if (!configuration.getDisableIfNameContains().contains(event.getVehicle().getWorld().getName()) && !configuration.getDisabledWorlds().contains(event.getVehicle().getWorld().getName())) {
                 if (configuration.getChunkLimit() <= Checker.countEntityInChunk(event.getVehicle().getChunk(), event.getVehicle().getType())) {
                     Checker.removeEntitiesInChunk(event.getVehicle().getChunk(), event.getVehicle().getType());
@@ -25,12 +26,28 @@ public class EntityListener implements Listener {
 
     @EventHandler
     public void onVehicleCreate(VehicleCreateEvent event) {
-        if (Checker.isChechedChunk(event.getVehicle().getChunk())) {
+        final EntityLimiterConfiguration configuration = EntityLimiter.getINSTANCE().getConfiguration();
+        if (configuration.getEntityType().contains(event.getVehicle().getType()) && Checker.isChechedChunk(event.getVehicle().getChunk())) {
             Checker.addChechedChunk(event.getVehicle().getChunk());
-            final EntityLimiterConfiguration configuration = EntityLimiter.getINSTANCE().getConfiguration();
             if (!configuration.getDisableIfNameContains().contains(event.getVehicle().getWorld().getName()) && !configuration.getDisabledWorlds().contains(event.getVehicle().getWorld().getName())) {
                 if (configuration.getChunkLimit() <= Checker.countEntityInChunk(event.getVehicle().getChunk(), event.getVehicle().getType())) {
                     Checker.removeEntitiesInChunk(event.getVehicle().getChunk(), event.getVehicle().getType());
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntitySpawnEvent(EntitySpawnEvent event) {
+        final EntityLimiterConfiguration configuration = EntityLimiter.getINSTANCE().getConfiguration();
+        if (configuration.getEntityType().contains(event.getEntity().getType())) {
+            event.getEntity().setGravity(false);
+            if (Checker.isChechedChunk(event.getEntity().getChunk())) {
+                Checker.addChechedChunk(event.getEntity().getChunk());
+                if (!configuration.getDisableIfNameContains().contains(event.getEntity().getWorld().getName()) && !configuration.getDisabledWorlds().contains(event.getEntity().getWorld().getName())) {
+                    if (configuration.getChunkLimit() <= Checker.countEntityInChunk(event.getEntity().getChunk(), event.getEntity().getType())) {
+                        Checker.removeEntitiesInChunk(event.getEntity().getChunk(), event.getEntity().getType());
+                    }
                 }
             }
         }
